663d7d080290c444053618d44a3e5bb8820cfd77
Kei <kkw@robotis.com>
2020-02-27 22:32:08 +0900

    Added driver code for clock, systick, gpio.

--- firmware/rt1064_evk/application/src/bsp/bsp.c
+++ firmware/rt1064_evk/application/src/bsp/bsp.c
@@ -1,6 +1,27 @@
 #include "bsp.h"
 
+#include "config/board/clock_config.h"
+#include "config/board/pin_mux.h"
+#include "config/board/peripherals.h"
+
 void bsp_init()
 {
+  BOARD_InitBootPins();
+  BOARD_InitBootClocks();
+  BOARD_InitBootPeripherals();
 
+  SysTick_Config(SystemCoreClock/1000U);
+}
+
+
+static volatile uint32_t systick_counter = 0;
+
+void SysTick_Handler(void)
+{
+  systick_counter++;
+}
+
+uint32_t get_systick_counter()
+{
+  return systick_counter;
 }
