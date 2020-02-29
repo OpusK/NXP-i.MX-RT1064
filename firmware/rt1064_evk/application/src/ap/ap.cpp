253308b287b022ea74596371424ba9a62120de2f
Kei <kkw@robotis.com>
2020-02-29 15:14:11 +0900

    Updated main ap code(LED, UART).

--- firmware/rt1064_evk/application/src/ap/ap.cpp
+++ firmware/rt1064_evk/application/src/ap/ap.cpp
@@ -1,17 +1,34 @@
 #include "ap.h"
 #include "bsp.h"
 
-#include "led_blinky/led_blinky.h"
+#include "bosdk.h"
 
 void apInit(void)
 {
   bsp_init();
+
+  //LED
+  ledInit();
+
+  //UART
+  uartInit();
+  uartOpen(0, 115200);
 }
 
 void apMain(void)
 {
+  static uint32_t pre_time;
+
   while(1)
   {
-    ap_led_blinky();
+    while(uartAvailable(0) > 0)
+    {
+      uartPutch(0, uartGetch(0));
+    }
+
+    if (millis()-pre_time >= 500){
+      pre_time = millis();
+      ledToggle(0);
+    }
   }
 }
